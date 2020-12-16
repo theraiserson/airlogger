4537ddf6593337fc4e47bed71e77de78ca917c0d
theraiserson <7570251+theraiserson@users.noreply.github.com>
2020-12-16 13:54:53 +0100

    Included Thingspeak. Now with different modes of publishing.

--- src/main.cpp
+++ src/main.cpp
@@ -26,17 +26,35 @@ void checkIaqSensorStatus(void);
 void errLeds(void);
 void setupWifi(void);
 void reconnectMqtt(void);
+void reconnectTS();
+void readSensors();
+void publish();
 void publishMqtt(String topic, String payload);
+void publishSerial();
+void publishMqtt();
+void publishTS();
+
+
+const bool use_mqtt = USE_MQTT;
+const bool use_ts = USE_TS;
 
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
 const char* mqtt_server = MQTT_SERVER_IP;
+const char* ts_username = TS_USERNAME;
+const char* ts_mqtt_api = TS_MQTT_API;
+const char* ts_channel_write_api = TS_CHANNEL_WRITE_API;
+const char* ts_server = TS_SERVER;
 
 const int pressureUpdateInterval = 60000 * 5; // every 5 minutes
-const int scd30MeasurementInterval = 5000; // every 5 seconds
+const int scdMeasurementInterval = SCD_MEASUREMENT_INTERVAL;
+
+const long ts_channel_id = TS_CHANNEL_ID;
+const unsigned long ts_publish_interval = TS_PUBLISH_INTERVAL*1000;
 
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
+PubSubClient tsClient(espClient);
 SCD30 scd30;
 Bsec iaqSensor;
 
@@ -64,7 +82,7 @@ void setup() {
         BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
     };
 
-    iaqSensor.updateSubscription(sensorList, 10, BSEC_SAMPLE_RATE_LP);
+    iaqSensor.updateSubscription(sensorList, 10, BSEC_SAMPLE_RATE_LP); //Low Power Mode, every 3 seconds
     checkIaqSensorStatus();
 
     // Start SCD30 after BSEC to allow it to
@@ -74,75 +92,40 @@ void setup() {
     } else {
         Serial.println("SCD30 not detected.");
     }
-    if (!scd30.setMeasurementInterval(scd30MeasurementInterval / 1000)) {
+    if (!scd30.setMeasurementInterval(scdMeasurementInterval)) {
         Serial.println("Failed to set SCD30 measurement interval");
-    }
+    }  
 }
 
 unsigned long lastPressureUpdate = 0;
 unsigned long lastScd30Check = 0;
+unsigned long now = 0;
+unsigned long lastTsPublish = 0;
+bool scd_new = false;
+bool iaq_new = false;
+
 
 void loop() {
-    if (!mqttClient.connected()) {
-        reconnectMqtt();
-    }
-    mqttClient.loop();
-    
-    unsigned long now = millis();
-    if (iaqSensor.run()) { // If new data is available
-        const float pressureHpa = iaqSensor.pressure / 100;
-        String output;
-        output = "BSEC: New data for timestamp " + String(now);
-        output += "\n raw temperature [°C]: " + String(iaqSensor.rawTemperature);
-        output += "\n pressure [hPa]: " + String(pressureHpa);
-        output += "\n raw relative humidity [%]: " + String(iaqSensor.rawHumidity);
-        output += "\n gas [Ohm]: " + String(iaqSensor.gasResistance);
-        output += "\n IAQ: " + String(iaqSensor.iaq);
-        output += "\n IAQ accuracy: " + String(iaqSensor.iaqAccuracy);
-        output += "\n temperature [°C]: " + String(iaqSensor.temperature);
-        output += "\n relative humidity [%]: " + String(iaqSensor.humidity);
-        output += "\n Static IAQ: " + String(iaqSensor.staticIaq);
-        output += "\n CO2 equivalent: " + String(iaqSensor.co2Equivalent);
-        output += "\n breath VOC equivalent: " + String(iaqSensor.breathVocEquivalent);
-        Serial.println(output);
-
-        publishMqtt("rawTemperature", String(iaqSensor.rawTemperature));
-        publishMqtt("pressure", String(pressureHpa));
-        publishMqtt("rawHumidity", String(iaqSensor.rawHumidity));
-        publishMqtt("gasResistance", String(iaqSensor.gasResistance));
-        publishMqtt("iaq", String(iaqSensor.iaq));
-        publishMqtt("iaqAccuracy", String(iaqSensor.iaqAccuracy));
-        publishMqtt("temperature", String(iaqSensor.temperature));
-        publishMqtt("humidity", String(iaqSensor.humidity));
-        publishMqtt("staticIaq", String(iaqSensor.staticIaq));
-        publishMqtt("co2Equivalent", String(iaqSensor.co2Equivalent));
-        publishMqtt("breathVocEquivalent", String(iaqSensor.breathVocEquivalent));
-        
-        if (now - lastPressureUpdate > pressureUpdateInterval) {
-            // Update ambient pressure
-            Serial.println("Updating SCD30 with new ambient pressure value [mbar]: " + String(pressureHpa));
-
-            if (!scd30.setAmbientPressure((uint16_t) pressureHpa)) {
-                Serial.println("Failed to update SCD30 with current ambient pressure");
-            }
-            lastPressureUpdate = now;
-        } ;
-    }
-
-    if (now - lastScd30Check > scd30MeasurementInterval) {
-        if (scd30.dataAvailable()) {
-            String scd30Co2 = String(scd30.getCO2());
-            Serial.println("SCD30: New data");
-            Serial.println(" co2(ppm): " + scd30Co2);
-            Serial.println(" temp(C): " + String(scd30.getTemperature()));
-            Serial.println(" humidity(%): " + String(scd30.getHumidity()));
-            Serial.println();
-            publishMqtt("co2", scd30Co2);
-        } else {
-            Serial.println("SCD30: No data.");
+    if (use_mqtt) {
+        if (!mqttClient.connected()) {
+            reconnectMqtt();
         }
-        lastScd30Check = now;
+        mqttClient.loop();
     }
+    
+    if (use_ts) {
+        if (!tsClient.connected())
+        {
+            reconnectTS();
+        }
+        tsClient.loop();        
+    }
+    
+    now = millis();
+    
+    readSensors();
+
+    publish();
 }
 
 void checkIaqSensorStatus(void) {
@@ -182,8 +165,121 @@ void checkIaqSensorStatus(void) {
     }
 }
 
+void readSensors(){
+    if (iaqSensor.run() ) {
+        iaq_new = true;
+
+        //Update SCD30 with new ambient pressure value (mbar)
+        if (now - lastPressureUpdate > pressureUpdateInterval) {
+            // Update ambient pressure
+            Serial.println("Updating SCD30 with new ambient pressure value [mbar].");
+            if (!scd30.setAmbientPressure((uint16_t) (iaqSensor.pressure / 100))) {
+                Serial.println("Failed to update SCD30 with current ambient pressure");
+            }
+            lastPressureUpdate = now;
+        }
+    }
+    if (scd30.dataAvailable()) {
+        scd_new = true;
+    }
+}
+
+void publish() {
+    if (iaq_new || scd_new) {    
+        if (PUBLISH_METHOD == 0 && iaq_new && scd_new)
+        {
+            publishSerial();
+            if (use_mqtt) publishMqtt();
+            if (use_ts) publishTS();                 
+        } else if (PUBLISH_METHOD == 1)
+        {
+            publishSerial();
+            if (use_mqtt) publishMqtt();
+            if (use_ts) publishTS();
+        } else if (PUBLISH_METHOD == 2)
+        {
+            publishSerial();
+            if (use_mqtt) publishMqtt();
+            if (use_ts) publishTS();
+        } 
+        iaq_new = false;
+        scd_new = false;       
+    }
+    
+}
+
 void publishMqtt(String topic, String payload) {
     mqttClient.publish(String(MQTT_TOPIC_PREFIX + topic).c_str(), payload.c_str());
+}
+
+void publishMqtt() {
+    if (PUBLISH_METHOD !=2 || iaq_new)
+    {   
+        publishMqtt("rawTemperature", String(iaqSensor.rawTemperature));
+        publishMqtt("pressure", String(iaqSensor.pressure / 100));
+        publishMqtt("rawHumidity", String(iaqSensor.rawHumidity));
+        publishMqtt("gasResistance", String(iaqSensor.gasResistance));
+        publishMqtt("iaq", String(iaqSensor.iaq));
+        publishMqtt("iaqAccuracy", String(iaqSensor.iaqAccuracy));
+        publishMqtt("temperature", String(iaqSensor.temperature));
+        publishMqtt("humidity", String(iaqSensor.humidity));
+        publishMqtt("staticIaq", String(iaqSensor.staticIaq));
+        publishMqtt("co2Equivalent", String(iaqSensor.co2Equivalent));
+        publishMqtt("breathVocEquivalent", String(iaqSensor.breathVocEquivalent));
+    }
+    if (PUBLISH_METHOD !=2 || scd_new) {
+        publishMqtt("co2", String(scd30.getCO2()));
+    }
+}
+
+void publishSerial() {
+    String output = "\ntimestamp" + String(now) + "\n";
+    if (PUBLISH_METHOD != 2) {
+        if (iaq_new) output += "New IAQ-Data!";
+        else output += "Old IAQ-Data.";
+        if (scd_new) output += " New SCD-Data!";
+        else output += " Old SCD-Data.";
+    } else if (iaq_new) output += "New IAQ-Data!";
+    else output += "New SCD-Data!";
+    if (PUBLISH_METHOD !=2 || iaq_new) {
+        output += "\n raw temperature [°C]: " + String(iaqSensor.rawTemperature);
+        output += "\n pressure [hPa]: " + String(iaqSensor.pressure / 100);
+        output += "\n raw relative humidity [%]: " + String(iaqSensor.rawHumidity);
+        output += "\n gas [Ohm]: " + String(iaqSensor.gasResistance);
+        output += "\n IAQ: " + String(iaqSensor.iaq);
+        output += "\n IAQ accuracy: " + String(iaqSensor.iaqAccuracy);
+        output += "\n temperature [°C]: " + String(iaqSensor.temperature);
+        output += "\n relative humidity [%]: " + String(iaqSensor.humidity);
+        output += "\n Static IAQ: " + String(iaqSensor.staticIaq);
+        output += "\n CO2 equivalent: " + String(iaqSensor.co2Equivalent);
+        output += "\n breath VOC equivalent: " + String(iaqSensor.breathVocEquivalent);
+    }
+    if (PUBLISH_METHOD !=2 || scd_new) {
+        output += "\n CO2(ppm) " + String(scd30.getCO2()) ;
+        output += "\n temperature [°C] " + String(scd30.getTemperature());
+        output += "\n relative humidity [%] " + String(scd30.getHumidity());
+    }
+    Serial.println(output);  
+}
+
+void publishTS() {
+    if (now - lastTsPublish > ts_publish_interval) {
+        String output;
+        output = "field1=" + String(iaqSensor.temperature);
+        output += "&field2=" + String(iaqSensor.pressure/100);
+        output += "&field3=" + String(iaqSensor.humidity);
+        output += "&field4=" + String(iaqSensor.staticIaq);
+        output += "&field5=" + String(iaqSensor.staticIaqAccuracy);
+        output += "&field6=" + String(iaqSensor.co2Equivalent);
+        output += "&field7=" + String(iaqSensor.co2Accuracy);
+        output += "&field8=" + String(scd30.getCO2());
+
+        String topic = "channels/" + String(ts_channel_id) + "/publish/" + String(ts_channel_write_api);
+        tsClient.publish(topic.c_str(), output.c_str());
+
+        lastTsPublish = now;
+    }
+
 }
 
 void setupWifi() {
@@ -208,6 +304,7 @@ void setupWifi() {
 
     Serial.println("WiFi connected");
     mqttClient.setServer(mqtt_server, 1883); //connecting to mqtt server
+    tsClient.setServer(ts_server, 1883);
 }
 
 void reconnectMqtt() {
@@ -220,6 +317,22 @@ void reconnectMqtt() {
     } else {
       Serial.print("failed, rc=");
       Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void reconnectTS() {
+    while (!tsClient.connected()) {
+    Serial.print("Attempting TS-MQTT connection...");
+    // Attempt to connect
+    if (tsClient.connect("airlogger",ts_username,ts_mqtt_api)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(tsClient.state());
       Serial.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
       delay(5000);
