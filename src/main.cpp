e60b75158738041745c0772fe6e534df240bcd92
Merlin Beutlberger <r@ndombyte.net>
2021-01-27 23:09:45 +0100

    Re-enable auto calibration

--- src/main.cpp
+++ src/main.cpp
@@ -88,13 +88,13 @@ void setup() {
 
     // Start SCD30 after BSEC to allow it to
     // configure clock streching correctly on ESP8266
-    if (scd30.begin(Wire, false)) {
+    if (scd30.begin(Wire, true /*auto calibration*/)) {
         Serial.println("SCD30 detected");
     } else {
         Serial.println("SCD30 not detected on first attempt");
         //Give it 2s to boot and then try again
         delay(2000);
-        if (scd30.begin(Wire, false)) {
+        if (scd30.begin(Wire, true /*auto calibration*/)) {
           Serial.println("SCD30 detected on second attempt");
         }
     }
